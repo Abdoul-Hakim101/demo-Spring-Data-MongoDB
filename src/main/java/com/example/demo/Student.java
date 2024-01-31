5e870f15af4814af75140f0d6123589eba464f0e
Abdihakim <abdihakin18@gmail.com>
2024-01-31 10:43:18 +0300

    Added annotations for MongoDB and Spring Data in Student class

--- src/main/java/com/example/demo/Student.java
+++ src/main/java/com/example/demo/Student.java
@@ -1,12 +1,17 @@
 package com.example.demo;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
+@Document
 public class Student {
+    @Id
+    private String id;
     private String firstName;
     private String lastName;
     private String email;
